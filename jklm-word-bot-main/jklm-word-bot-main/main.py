f18826e1c57e5f18416a638e9f31dbd8e5e1621b
konrad <102168613+wiqqend@users.noreply.github.com>
2026-08-27 09:37:22 -0400

    Add files via upload

---
+++ jklm-word-bot-main/jklm-word-bot-main/main.py
@@ -0,0 +1,70 @@
+from pynput.keyboard import Listener, Key
+import pyautogui
+import pyperclip
+import random
+import time
+import sys
+import os
+
+bomb_x = ""
+bomb_y = ""
+delays = [0.03, 0.04, 0.05, 0.06, 0.2]
+long_words = True
+instant_typing = False
+pyautogui.PAUSE = 0
+used_words = set()
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(SCRIPT_DIR, 'wordlist.txt')) as word_file:
+    valid_words = word_file.read().split()
+
+def release(key):
+    global bomb_x, bomb_y, used_words
+    if key == Key.f8:
+        try:
+            bomb_x, bomb_y = pyautogui.position()
+        except Exception as err:
+            print(f"Error getting mouse position: {err}")
+    if key == Key.f4:
+        try:
+            pyautogui.click(x=bomb_x, y=bomb_y, clicks=2)
+            with pyautogui.hold('ctrl'):
+                pyautogui.press('c')
+            pyautogui.click(x=bomb_x - 100, y=bomb_y)
+            time.sleep(0.1)
+            syllable = pyperclip.paste().lower().strip()
+            pyperclip.copy('')
+            found_words = [word for word in valid_words if syllable in word]
+            
+            if not found_words:
+                print("No words were found.")
+                return
+
+            if long_words:
+                found_words.sort(key=len, reverse=True)
+            
+            final_word = random.choice(found_words)
+            while final_word in used_words:
+                if len(used_words) == len(found_words):
+                    print("All words have been used.")
+                    return
+                final_word = random.choice(found_words)
+            
+            used_words.add(final_word)
+            
+            if instant_typing:
+                pyperclip.copy(final_word)
+                with pyautogui.hold('ctrl'):
+                    pyautogui.press('v')
+            else:
+                for char in final_word:
+                    delay = random.choice(delays)
+                    pyautogui.write(char, delay)
+            time.sleep(0.1)
+            pyautogui.press('enter')
+        except Exception as e:
+            print(f"Error: {e}")
+
+with Listener(on_release=release) as listener:
+    listener.join()
